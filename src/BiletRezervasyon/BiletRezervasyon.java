d48dce9a67bc9a7e7db15a2cf36a88bbd329e1b0
İlker Şelimen <ilkerselimen@gmail.com>
2023-02-14 00:38:15 +0300

    v01

--- src/BiletRezervasyon/BiletRezervasyon.java
+++ src/BiletRezervasyon/BiletRezervasyon.java
@@ -1,5 +1,7 @@
 package BiletRezervasyon;
 
+import java.util.Scanner;
+
 /*
 Project: mesafeye ve şartlara göre otobüs bileti fiyatı hesaplayan uygulama
          Kullanıcıdan Mesafe (KM), yaşı ve yolculuk tipi (Tek Yön, Gidiş-Dönüş)
@@ -19,6 +21,41 @@ Project: mesafeye ve şartlara göre otobüs bileti fiyatı hesaplayan uygulama
         Kişi 65 yaşından büyük ise son bilet fiyatı üzerinden %30 indirim uygulanır.
 
  */
-
 public class BiletRezervasyon {
+    public static void main(String[] args) {
+        //1-bilet rezervasyonu için otobüs objesi oluşturalım.
+        Bus bus=new Bus("34 ASD 78");//"1","2"....
+        //4-bilet objesi oluşturalım.
+        Bilet bilet=new Bilet();
+    }
+    public static void start(Bus bus){
+        Scanner scan=new Scanner(System.in);
+        do {
+            //6-kullanıcıdan bilgileri alalım.
+            System.out.println("Bilet Rezervasyon Sistemine Hoşgeldiniz...");
+            System.out.println("Lütfen gidilecek mesafe bilgisini KM olarak giriniz:");
+            double distance=scan.nextDouble();
+            System.out.println("Lütfen yolculuk tipini seçiniz:");
+            System.out.println("1.Tek Yön");
+            System.out.println("2.Gidiş-Dönüş");
+            int type=scan.nextInt();
+            System.out.println("Lütfen yaşınızı giriniz:");
+            int age= scan.nextInt();
+            System.out.println("Koltuk no seçiniz:");
+            System.out.println("(Tekli koltuk ücreti %20 daha fazladır.)");
+            System.out.println(bus.seats);
+            int seat=scan.nextInt();
+            //7-seçilen koltuk noyu listeden kaldıralım.
+            bus.seats.remove(String.valueOf(seat));//"3"->"1","2","4"
+            //8-kullanıcıdan alınan değerler geçerli mi
+            boolean check=type==1 || type==2;
+            if(distance>0 && age>0 && check){
+                //9-bilet fiyatını hesaplayalım
+            }else{
+                System.out.println("Hatalı giriş yaptınız!");
+            }
+
+        }while();
+    }
+
 }
