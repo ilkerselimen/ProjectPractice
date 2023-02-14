963d07652f28739bc391f93c43b05ee3be5f6c56
İlker Şelimen <ilkerselimen@gmail.com>
2023-02-15 00:30:33 +0300

    v02

--- src/LoginpageApp/User.java
+++ src/LoginpageApp/User.java
@@ -6,13 +6,12 @@ public class User {
     private String username;
     private String email;
     private String password;
-
     //4-user objesi oluşturulurken öz. set edilsin.
-    public User(String name, String username, String email, String password) {
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.password = password;
+    public User(String name,String username,String email,String password){
+        this.name=name;
+        this.username=username;
+        this.email=email;
+        this.password=password;
     }
 
     public String getName() {
