d220c1270d1e02abba46b74d0f6ab61b1bee4b17
İlker Şelimen <ilkerselimen@gmail.com>
2023-02-17 01:31:56 +0300

    v04

---
+++ src/MiniBookStore/ProductService.java
@@ -0,0 +1,9 @@
+package MiniBookStore;
+//2-product işlemleri için standart
+public interface ProductService {
+    void processMenu();
+    void listProduct();
+    void  addProduct();
+    void deleteProduct();
+    void filterProducts(String filter);
+}
