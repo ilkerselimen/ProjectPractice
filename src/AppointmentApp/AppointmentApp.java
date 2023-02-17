bcb318f50435e368ba2ac4bd5e90407a72d8d24a
İlker Şelimen <ilkerselimen@gmail.com>
2023-02-18 02:07:51 +0300

    v05

---
+++ src/AppointmentApp/AppointmentApp.java
@@ -0,0 +1,22 @@
+package AppointmentApp;
+/*
+Proje: Randevu oluşturma uygulaması geliştiriniz.
+
+       1-Ana menüde kullanıcıya randevu alma veya görüntülemek için iki seçenek sunulur.
+       2-Randevu alma/oluşturma
+            -Randevu için uygun tarihler gösterilir.
+            -Randevu alınmak istenen tarihin seçilmesi istenir.
+            -en yakın 7 günlük takvim dolmuşsa daha sonra denenmesi istenir.
+            -Geçerli bir tarih seçilmişse randevu oluşturulur ve bu tarih tarihler listesinden silinir.
+       3-Randevu görüntüleme:
+            -Kullanıcıya randevu no sorulur,
+             randevu listesinde girilen no ile eşleşen randevu bilgileri yazdırılır.
+       4-Randevu iptal:opsiyonel
+            -Kullanıcıya randevu no sorulur,
+             randevu listesinde girilen no ile eşleşen randevu listeden silinir.
+       5-Randevu:
+            -id : otomatik üretilsin
+            -isim,randevu tarihi
+ */
+public class AppointmentApp {
+}
