bd26dd263859b5f25cad73828592355846b81fa2
İlker Şelimen <ilkerselimen@gmail.com>
2023-02-18 02:04:01 +0300

    v04+

--- src/MiniBookStore/BookService.java
+++ src/MiniBookStore/BookService.java
@@ -72,19 +72,79 @@ public class BookService implements ProductService{
         System.out.println("--------------------------------------------------------------------------");
         System.out.println();
     }
-
+    //7:yeni kitap ekle
     @Override
     public void addProduct() {
-
+        System.out.println("ISBN: ");
+        String isbn=inp.nextLine();
+        boolean isExists=false;
+        for(Book book:this.books){
+            if(book.getIsbn().equals(isbn)){
+                System.out.println("Bu kitap sistemde zaten kayıtlı, lütfen ürün güncelleme yapınız.");
+                isExists=true;
+                break;
+            }
+        }
+        if (!isExists){
+            System.out.println("Kitap adı:");
+            String name=inp.nextLine();
+            System.out.println("Yazar adı:");
+            String author=inp.nextLine();
+            System.out.println("Yayınevi: ");
+            String publisher=inp.nextLine();
+            System.out.println("Birim Fiyatı: ");
+            String price=inp.nextLine();
+            System.out.println("Stok :");
+            int stock=inp.nextInt();
+            inp.nextLine();
+            Book newBook=new Book(name,price,stock,author,publisher,isbn);
+            this.books.add(newBook);
+        }
+        listProduct();
     }
+    //updateProduct:stok artırma/azaaltma,birim fiyat:ÖDEV
 
+    //8:kullanıcıdan alınan id ile ürünü bulalım ve listeden silelim
     @Override
     public void deleteProduct() {
-
+        boolean isExists=true;
+        System.out.println("Kitap id: ");
+        int id=inp.nextInt();
+        for (Book book:this.books){
+            if (book.getId()==id){
+                isExists=true;
+                this.books.remove(book);
+                System.out.println("Ürün silindi.");
+                break;
+            }else{
+                isExists=false;
+            }
+        }
+        if(!isExists){
+            System.out.println("Ürün bulunamadı.");
+        }
     }
-
+    //9:kitapları yayınevine göre filtrele
     @Override
     public void filterProducts(String filter) {
+//        this.books.stream().
+//                filter(book->book.getPublisher().equalsIgnoreCase(filter)).
+//                forEach(book ->System.out.printf("%-2s | %-20s | %-15s | %-10s | %-4s | %-10s | %-3s\n",
+//                        book.getId(),book.getName(),book.getAuthorName(),book.getPublisher(),book.getIsbn(),
+//                        book.getPrice(),book.getStock()));
+//
+        int counter=0;
+        for (Book book:this.books){
+            if(book.getPublisher().equalsIgnoreCase(filter)){
+                System.out.printf("%-2s | %-20s | %-15s | %-10s | %-4s | %-10s | %-3s\n",
+                        book.getId(),book.getName(),book.getAuthorName(),book.getPublisher(),book.getIsbn(),
+                        book.getPrice(),book.getStock());
+                counter++;
+            }
+        }
+        if(counter==0){
+            System.out.println("Ürün bulunamadı.");
+        }
 
     }
 }
